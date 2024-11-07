a7c5e00ec649add619f3ebe91d71a0e225543c0a
davidenrique <davidenriquea@icloud.com>
2024-11-07 20:02:26 +0100

    Initial commit

---
+++ src/main/java/org/dis/back/BRException.java
@@ -0,0 +1,10 @@
+package com.ufv.dis;
+//Excepción genérica para el ejercicio.
+//Como extiende a Exception, hereda todos los métodos necesarios
+//para funcionar correctamente en try-catch o throw
+
+public class BRException extends Exception{
+    public BRException(String causa) {
+        super (causa);
+    }
+}
